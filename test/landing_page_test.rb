1961bf8615a8ec1c565615014dfe636f59876ffd
marthaberner <martha@marthaberner.com>
2014-03-05 20:45:01 -0700

    added contact link to footer
    used mailto #

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -17,7 +17,7 @@ describe "Landing page" do
   end
 
   it "displays two paragraphs of welcome text" do
-  skip
+skip
     welcome_text = page.all('html body main p')
     welcome_text.length.must_equal 2
 
@@ -27,7 +27,7 @@ describe "Landing page" do
   end
 
   it "displays a list of names for 5 dishes" do
-    skip
+skip
     dishes = page.all('html body main ul li')
     dishes.length.must_equal 5
 
